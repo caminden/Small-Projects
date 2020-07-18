6434d26a8fc1e182b1321b908e24162b29133b89
caminden <58918442+caminden@users.noreply.github.com>
2020-07-17 20:56:33 -0500

    Create Magic8.cpp

---
+++ Magic8.cpp
@@ -0,0 +1,22 @@
+//Magic 8 ball
+#include <iostream>;
+using namespace std;
+
+int main() {
+
+	//char val = 's';
+	/*while (val != 'q') {
+		int n = rand() % 10;
+		cout << "Enter question or quit to quit" << endl;
+		cin >> val;
+		if (val == 'q') {
+			cout << "quitting..." << endl;
+		}
+		else {
+			cout << "it worked!" << endl;
+		}
+		
+	}*/
+cout << "Working" << endl;
+return 0;
+}
