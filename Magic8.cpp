dd47d11a07579458c59dd3081a4a7b87d38838fe
chase minden <caminden@gmail.com>
2020-08-19 15:10:02 -0500

    Finish Magic8.cpp with executable

--- Magic8.cpp
+++ Magic8.cpp
@@ -1,22 +1,56 @@
 //Magic 8 ball
-#include <iostream>;
+
+#include <iostream>
+#include <string>
 using namespace std;
 //work in progress
 int main() {
 
-	//char val = 's';
-	/*while (val != 'q') {
+
+	string val = "start";
+
+	while (val != "quit") {
 		int n = rand() % 10;
 		cout << "Enter question or quit to quit" << endl;
-		cin >> val;
-		if (val == 'q') {
+		getline(cin, val);
+
+		if (val == "quit") {
 			cout << "quitting..." << endl;
 		}
 		else {
-			cout << "it worked!" << endl;
+			if(n == 1){
+				cout << "Yes" << endl;
+			}
+			if(n == 2){
+				cout << "No" << endl;
+			}
+			if(n == 3){
+				cout << "Maybe" << endl;
+			}
+			if(n == 4){
+				cout << "Maybe not" << endl;
+			}
+			if(n == 5){
+				cout << "Quite possibly" << endl;
+			}
+			if(n == 6){
+				cout << "Not possible" << endl;
+			}
+			if(n == 7){
+				cout << "Ask another program" << endl;
+			}
+			if(n == 8){
+				cout << "Try again" << endl;
+			}
+			if(n == 9){
+				cout << "IDK Im no expert" << endl;
+			}
+			if(n == 0){
+				cout << "%$ERROR$%" << endl;
+			}
 		}
 		
-	}*/
-cout << "Working" << endl;
+	}
+	cin.get();
 return 0;
 }
